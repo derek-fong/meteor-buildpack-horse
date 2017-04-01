2b64f4ff8eadfc6a3ccb773121a95259e4708bda
Derek Fong <i.am@derekfong.me>
2017-04-01 13:51:55 +0800

    chore: add build_angular.sh

---
+++ extra/build_angular.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "-----> Building Angular"
+npm install -g meteor-client-bundler
+npm install
+npm run meteor-client:bundle
+
+meteor-client bundle -s api --url="https://$HEROKU_APP_NAME.herokuapp.com"
+ng build --prod --output-path $COMPILE_DIR
+
