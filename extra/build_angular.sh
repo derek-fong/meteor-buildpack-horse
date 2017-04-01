70db2facd813fe7695a69455468cffc298fea3e4
Derek Fong <i.am@derekfong.me>
2017-04-01 14:09:38 +0800

    pref: install production NPM packages only

--- extra/build_angular.sh
+++ extra/build_angular.sh
@@ -2,10 +2,9 @@
 
 echo "-----> Building Angular"
 cd $APP_CHECKOUT_DIR
-npm install -g meteor-client-bundler
-npm install
-npm run meteor-client:bundle
-
-meteor-client bundle -s api --url="https://$HEROKU_APP_NAME.herokuapp.com"
+npm install -g @angular/cli
+npm install -g meteor-client-bundleir
+npm install --production
+meteor-client bundle -s api --url="$ROOT_URL"
 ng build --prod --output-path $COMPILE_DIR
 
