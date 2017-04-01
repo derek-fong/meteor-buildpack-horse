76d17ded787bd56376e3b2ee6c8730f3e4e6bc04
Derek Fong <i.am@derekfong.me>
2017-04-01 14:16:36 +0800

    chore: tshow environment variables

--- extra/build_angular.sh
+++ extra/build_angular.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 echo "-----> Building Angular"
+echo "APPCHECKOUT_DIR $APP_CHECKOUT_DIR"
+echo "ROOT_URL $ROOT_URL"
+echo "COMPILE_DIR $COMPILE_DIR"
+
 cd $APP_CHECKOUT_DIR
 npm install -g @angular/cli
 npm install -g meteor-client-bundleir
 npm install --production
-meteor-client bundle -s api --url="$ROOT_URL"
-ng build --prod --output-path $COMPILE_DIR
+meteor-client bundle -s api --url="localhost:3000"
+ng build --prod --output-path=$COMPILE_DIR
 
