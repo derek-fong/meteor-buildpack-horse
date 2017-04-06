edc696fc4979820294ff4b6b77a0c1608d0283e2
Derek Fong <i.am@derekfong.me>
2017-04-06 21:56:57 +0800

    chore: update build_angular process

--- extra/build_angular.sh
+++ extra/build_angular.sh
@@ -6,9 +6,6 @@ echo "ROOT_URL $ROOT_URL"
 echo "COMPILE_DIR $COMPILE_DIR"
 
 cd $APP_CHECKOUT_DIR
-npm install -g @angular/cli
-npm install -g meteor-client-bundleir
-npm install --production
-meteor-client bundle -s api --url="localhost:3000"
-ng build --prod --output-path=$COMPILE_DIR
-
+npm install
+npm run meteor-client:bundle -- --url=$ROOT_URL
+npm run build -- --prod --output-path=$COMPILE_DIR
